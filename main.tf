41a865665509c6b3a8628f3f08c5b9b75adf68a3
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-09 22:44:47 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "ec2_module" {
+    source = ".terraform-aws-instance"
+        ami_id = var.ami_id
+    # sg_ids = var.sg_ids
+    instance_type = var.instance_type
+    tags = var.tags
+
+}
+
+output "pub_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "priv_ip" {
+    value = module.catalogue.private_ip
+}
+
+output "id" {
+    value = module.catalogue.instance_id
+}
