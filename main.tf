7b2d4511655b31b47e9c0941743bb5454fe69644
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-09 22:47:26 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ec2_module" {
-    source = "../terraform-aws-instance"
+    source = "../terraform-aws-instance"        #Importing the module from the local folder or dir
         ami_id = var.ami_id
     # sg_ids = var.sg_ids
     instance_type = var.instance_type
