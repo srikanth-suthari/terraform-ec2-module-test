4bba65a0b546f6e107b600e368112924a846b505
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-15 21:35:52 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,23 @@
 module "ec2_module" {
     source = "../terraform-aws-instance"        #Importing the module from the local folder or dir
-        ami_id = var.ami_id
+    ami_id = var.ami_id
     # sg_ids = var.sg_ids
     instance_type = var.instance_type
     tags = var.tags
-
 }
 
-output "pub_ip" {
-    value = module.catalogue.public_ip
+output "public_ip" {
+    value = module.ec2_module.ec2_public_ip
 }
 
-output "priv_ip" {
-    value = module.catalogue.private_ip
-}
+# output "pub_ip" {
+#     value = module.catalogue.public_ip
+# }
 
-output "id" {
-    value = module.catalogue.instance_id
-}
+# output "priv_ip" {
+#     value = module.catalogue.private_ip
+# }
+
+# output "id" {
+#     value = module.catalogue.instance_id
+# }
