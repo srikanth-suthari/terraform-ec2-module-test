0246ce8cbb269a5b689006f1aada3e70fcccaad9
srikanth-suthari <suthari.srikanth@outlook.com>
2026-09-16 21:51:28 +0530

    terraform

--- main.tf
+++ main.tf
@@ -10,10 +10,14 @@ output "public_ip" {
     value = module.ec2_module.ec2_public_ip
 }
 
-output "instance_id" {
+output "instance_type" {
     value = module.ec2_module.ec2_instance_type
 }
 
+
+output "instance_id" {
+    value = aws_instance.this.ec2_instance_id
+}
 # output "pub_ip" {
 #     value = module.catalogue.public_ip
 # }
